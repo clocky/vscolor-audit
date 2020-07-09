5f555ff3189000df5b2eecbd0d5cdc68bcefc028
Mark McLaughlin <mark.mclaughlin@microsoft.com>
2020-07-08 17:47:23 -0700

    Unused imports

--- theme.py
+++ theme.py
@@ -1,7 +1,7 @@
 from collections import Counter
 from tabulate import tabulate
 from colour import Color
-from colored import fg, bg, attr
+from colored import bg, attr
 from operator import itemgetter
 
 # Read all the ARGB values from a text file
@@ -17,7 +17,8 @@ for argb, count in counter:
     # Create color object from the RGB values in ARGB
     color = Color('#' + argb[3:10])
     table.append((count, bg(color.hex_l) + "        " +
-                  attr('reset'), argb, color.hex_l, color.hue, color.luminance))
+                  attr('reset'), argb, color.hex_l,
+                  color.hue, color.luminance))
 
 # Sort the table by hue, then luminance
 by_hue = sorted(table, key=itemgetter(4, 5))
